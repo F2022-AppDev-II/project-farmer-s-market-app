1524e0051090ee038f93d4bebd8571b3dcf63c6e
Sarim <a.sarimsyed@gmail.com>
2022-11-30 07:13:34 -0500

    Setting options remembered and menu added

--- project/app/src/main/java/com/example/farmersmarketapp/MainActivity.java
+++ project/app/src/main/java/com/example/farmersmarketapp/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.farmersmarketapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +18,7 @@ import com.example.farmersmarketapp.db.FarmerViewModel;
 import com.example.farmersmarketapp.utils.adapter.ProductItemAdapter;
 import com.example.farmersmarketapp.utils.model.ProductItem;
 import com.example.farmersmarketapp.views.DetailedActivity;
+import com.example.farmersmarketapp.views.SettingsActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +43,33 @@ public class MainActivity extends AppCompatActivity implements ProductItemAdapte
         productAdapter.setProductItems(productItems);
         recyclerView.setAdapter(productAdapter);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Toast.makeText(this, "Settings not working", Toast.LENGTH_SHORT);
+
+        switch (item.getItemId()){
+            case R.id.settingsOption:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT);
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+
+                return false;
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 
     private  void setUpList(){
         productItems = new ArrayList<>();
@@ -67,6 +101,8 @@ public class MainActivity extends AppCompatActivity implements ProductItemAdapte
 
 
     }
+
+
 
     @Override
     public void onCardClicked(ProductItem productItem) {
