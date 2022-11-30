d4edffb8e784b7379631dc5e2fd97432ae0a4e0c
Gorav-K <78222737+Gorav-K@users.noreply.github.com>
2022-11-29 20:25:13 -0500

    fix detail Page

--- project/app/src/main/java/com/example/farmersmarketapp/MainActivity.java
+++ project/app/src/main/java/com/example/farmersmarketapp/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
